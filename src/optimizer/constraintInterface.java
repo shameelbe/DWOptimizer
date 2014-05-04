4053752da3961cd71b47e29142de7d4b22d34d13
Shameel <Shameel@172.29.81.236>
2014-05-04 14:48:28 +0930

    Add constraint Interface

---
+++ src/optimizer/constraintInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package optimizer;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Shameel
+ */
+public interface constraintInterface {
+    public double checkConstraints(ArrayList<Double> bagpipeVals);
+}
