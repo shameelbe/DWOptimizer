dd96dd36d2508926d2a131156e27320ca251c1ad
Shameel <Shameel@172.29.81.236>
2014-05-05 23:51:50 +0930

    Added JAVA DOCS

--- src/optimizer/Input.java
+++ src/optimizer/Input.java
@@ -1,59 +1,57 @@
 package optimizer;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.List;
 import javax.swing.JOptionPane;
 import org.abstractmeta.toolbox.compilation.compiler.JavaSourceCompiler;
 import org.abstractmeta.toolbox.compilation.compiler.impl.JavaSourceCompilerImpl;
 
 /**
+ * The Class is responsible for setting up the input provided by user for
+ * calculation. The profit and constraint functions are provided as String by
+ * Users. It is converted into function (creating JAVA code and compiling it at
+ * runtime). Reference : We used the free open source JAVA LIBRARY for JAVA code
+ * Compilation at runtime. Link :
+ * https://code.google.com/p/compilation-toolbox/downloads/list
  *
- * @author smriti
+ * @author Smriti Batra and Shameel Mohamed
  */
 public class Input {
+    //member variables
 
     String profit;
-
-    public void setProfit(String profit) {
-        this.profit = profit;
-    }
-    ArrayList<Constraints> constraints = new ArrayList<Constraints>();
-
-    public void setConstraints(ArrayList<Constraints> constraints) {
-        this.constraints = constraints;
-    }
-    String profitCalPath;
-    String constCalPath;
-    OutputStream stdin = null;
-    InputStream stderr = null;
-    InputStream stdout = null;
-    int numberOfVariables = 3; //hard coding
-    //static String profit = "5+7";
+    private int numberOfVariables = 0;
     JavaSourceCompiler.CompilationUnit compilationUnit;
     JavaSourceCompiler javaSourceCompiler;
     String content_profit;
     profitInterface pFunction;
+    private ArrayList<Constraints> constraints = new ArrayList<Constraints>();
+    // setter methods
 
+    public void setProfit(String profit) {
+        this.profit = profit;
+    }
+
+    public void setConstraints(ArrayList<Constraints> constraints) {
+        this.constraints = constraints;
+    }
+
+    /**
+     * Constructor initializes the variables required to compile runtime
+     *
+     * @return void
+     */
     Input() {
         javaSourceCompiler = new JavaSourceCompilerImpl();
         compilationUnit = javaSourceCompiler.createCompilationUnit();
     }
 
-    Input(String profit, ArrayList<Constraints> constraints) {
-        this.profit = profit;
-        this.constraints = constraints;
-
-    }
-
     /**
-     * This method creates and compiles the profit calculator
+     * This method creates and compiles the profit calculator It creates a JAVA
+     * code and compiles it in runtime to compute the profit function provided
+     * as String.
      *
+     * @return void
      * @throws IOException
      */
     public void genProfitCalc() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
@@ -85,17 +83,31 @@ public class Input {
 
     }
 
+    /**
+     * It calls the function compute profit from ProfitCalc created at runtime
+     * to compute profit and returns the profit value.
+     *
+     * @return double returns the computed profit value.
+     * @throws IOException
+     */
     public double returnProfit(double[] bagpipeVals) {
-        System.out.println(pFunction.computeProfit(bagpipeVals));
+
         return pFunction.computeProfit(bagpipeVals);
     }
 
+    /**
+     * Validates the input. Check Whether COnstraints are specified for all
+     * input variables and also calculates the number of input variables and set
+     * the value for no of variables.
+     *
+     * @return boolean true if validation passes else false.
+     * @throws IOException
+     */
     public boolean validateInput() {
         ArrayList<String> inputVars = new ArrayList<String>();
         int i = 0;
         boolean flag = false;
-        //for(int j=0; j<numberOfVariables; j++)
-        System.out.println("Profit length: " + profit.length());
+
         int out = 0;
         while (out <= profit.length() - 1) {
 
@@ -103,8 +115,6 @@ public class Input {
             if (i >= 0) {
                 if (!inputVars.contains(profit.substring(i, i + 4))) {
                     inputVars.add(profit.substring(i, i + 4));
-                    System.out.println("i value: " + i);
-                    System.out.println("Input Variable is : " + profit.substring(i, i + 4));
                 }
             }
             out = i + 4;
@@ -117,8 +127,6 @@ public class Input {
 
             for (Constraints s1 : constraints) {
                 String cons = s1.getConstraint();
-                System.out.println("s1");
-                System.out.println("cons");
                 if (cons.contains(s)) {
                     flag = true;
 
@@ -126,8 +134,6 @@ public class Input {
             }
 
             if (flag == false) {
-                System.out.println("Not even one constraint present on " + s);
-                System.out.println("Inputs not validated..");
                 JOptionPane.showMessageDialog(null, "Constraints are not defined for all variables in Profit Function", "Error", JOptionPane.ERROR_MESSAGE);
                 return false;
             }
@@ -145,8 +151,13 @@ public class Input {
      * @throws IOException
      */
     /**
-     * This method creates and compiles the constraint calculator
+     * This method creates and compiles the constraint calculator It creates a
+     * JAVA code and compiles it in runtime to compute the COnstraint functions
+     * provided as String. It computes penalty for all constraint violation and
+     * returns total penalty value.
      *
+     * @param doube[] array of different bagpipe values
+     * @return double returns the computed total penalty value.
      * @throws IOException
      */
     public double genConstraintCalc(double[] bagpipeVals) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
@@ -199,73 +210,11 @@ public class Input {
 
             pTotal = pTotal + pValue;
         }
-        System.out.println(pTotal);
         return pTotal;
     }
 
-    /**
-     * This method calculates the profit for each production schedule
-     */
-    public void computeProfit(ArrayList<Double> bagpipeVal) throws IOException {
-        String[] cmd = {"java",
-            "-classpath",
-            "C:\\Program Files\\RuntimeTest\\ProfitCalc", bagpipeVal.get(0).toString(),
-            bagpipeVal.get(1).toString(), bagpipeVal.get(2).toString(),
-            bagpipeVal.get(3).toString(), bagpipeVal.get(4).toString(),
-            bagpipeVal.get(5).toString(), bagpipeVal.get(6).toString(),
-            bagpipeVal.get(7).toString(), bagpipeVal.get(8).toString(),
-            bagpipeVal.get(9).toString()
-        };
-        Process process = Runtime.getRuntime().exec(cmd);
-
-        stdin = process.getOutputStream();
-        stderr = process.getErrorStream();
-        stdout = process.getInputStream();
-
-        String profit1 = stdin.toString();
-    }
-
-    /**
-     * This method calculates the total penalty that applies on a schedule
-     */
-    public void computePenalty(ArrayList<Double> bagpipeVal) throws IOException {
-        String[] cmd = {"java",
-            "-classpath",
-            "C:\\Program Files\\RuntimeTest\\ConstraintCalc", bagpipeVal.get(0).toString(),
-            bagpipeVal.get(1).toString(), bagpipeVal.get(2).toString(),
-            bagpipeVal.get(3).toString(), bagpipeVal.get(4).toString(),
-            bagpipeVal.get(5).toString(), bagpipeVal.get(6).toString(),
-            bagpipeVal.get(7).toString(), bagpipeVal.get(8).toString(),
-            bagpipeVal.get(9).toString()
-        };
-        Process process = Runtime.getRuntime().exec(cmd);
-
-        stdin = process.getOutputStream();
-        stderr = process.getErrorStream();
-        stdout = process.getInputStream();
-
-    }
-
-    public void calcNoOfVariables() {
-    }
-
+    // getter method
     public int getNoOfVariables() {
         return this.numberOfVariables;
-    }
-
-    public String getProfitCalPath() {
-        return profitCalPath;
-    }
-
-    public void setProfitCalPath(String profitCalPath) {
-        this.profitCalPath = profitCalPath;
-    }
-
-    public String getConstCalPath() {
-        return constCalPath;
-    }
-
-    public void setConstCalPath(String constCalPath) {
-        this.constCalPath = constCalPath;
     }
 }
